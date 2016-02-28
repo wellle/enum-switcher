df8e825c0ad0751424074b66f76e6d76e7ca4965
Christian Wellenbrock <christian.wellenbrock@gmail.com>
2016-02-29 00:02:47 +0100

    Sort cases by character for -format=c

--- format_c.go
+++ format_c.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"sort"
 )
 
 const tab = "    "
@@ -21,19 +22,58 @@ func formatC(strategy *Strategy, lineIndexes map[string]int, indent string) stri
 	buffer.WriteString(fmt.Sprintf("switch(s[%d]) {\n", strategy.Index))
 
 	// add simple cases
-	for r, line := range strategy.Lines {
-		buffer.WriteString(fmt.Sprintf("%s%scase %s: return %d; // %s\n", indent, tab, char(r), lineIndexes[line], line))
+	runes := Runes{}
+	for r, _ := range strategy.Lines {
+		runes = append(runes, r)
+	}
+	sort.Sort(runes)
+	for _, r := range runes {
+		line := strategy.Lines[r]
+		buffer.WriteString(fmt.Sprintf(
+			"%s%scase %s: return %d; // %s\n",
+			indent,
+			tab,
+			char(r),
+			lineIndexes[line],
+			line,
+		))
 	}
 
 	// add recursive cases
-	for r, strat := range strategy.Strategies {
-		buffer.WriteString(fmt.Sprintf("%s%scase %s: %s", indent, tab, char(r), formatC(strat, lineIndexes, indent+tab)))
+	runes = Runes{}
+	for r, _ := range strategy.Strategies {
+		runes = append(runes, r)
+	}
+	sort.Sort(runes)
+	for _, r := range runes {
+		strat := strategy.Strategies[r]
+		buffer.WriteString(fmt.Sprintf(
+			"%s%scase %s: %s",
+			indent,
+			tab,
+			char(r),
+			formatC(strat, lineIndexes, indent+tab),
+		))
 	}
 
 	// close switch statement
 	buffer.WriteString(fmt.Sprintf("%s%sdefault: return -1;\n", indent, tab))
 	buffer.WriteString(fmt.Sprintf("%s}\n", indent))
 	return buffer.String()
+}
+
+type Runes []rune
+
+func (r Runes) Len() int {
+	return len(r)
+}
+
+func (r Runes) Swap(i, j int) {
+	r[i], r[j] = r[j], r[i]
+}
+
+func (r Runes) Less(i, j int) bool {
+	return r[i] < r[j]
 }
 
 func char(r rune) string {
